a8432c5c2cabe7d884f85a1b852f2135d3d81306
Alex Szymanski <alex.szy.2@gmail.com>
2026-09-19 19:10:44 -0400

    lab fully built, all tests passing, calathea added matching demo.gif

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => {
+        if (!r.ok) throw new Error("Failed to fetch plants");
+        return r.json();
+      })
+      .then((data) => setPlants(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants((plants) => [...plants, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
